325f30253aff0a62bc9ffd60f5010589cf28175c
sumit pandey <sumit1234coc@gmail.com>
2024-11-12 23:47:34 +0530

    first commit

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ connectToMongo();
 const path = require('path');
 const express = require('express');
 const app = express()
+const port=process.env.PORT || 5000
+ 
 
 app.use('/files', express.static(path.join(__dirname, '../files/image')));
 app.use(express.json()); 
 
 
-app.use(cors());  
-app.options('*', cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'], 
+}));
+
+ // Apply CORS with the specified options
+app.options('*', cors());  
+
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 app.use(express.urlencoded({extended:false}))
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('Server is running on http://localhost:5000');
 });
 
