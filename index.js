cccb12e5a94757750a682b98605ba4da9c04cc1b
sumit pandey <sumit1234coc@gmail.com>
2024-11-12 17:53:51 +0530

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ connectToMongo();
 const path = require('path');
 const express = require('express');
 const app = express()
+
 app.use('/files', express.static(path.join(__dirname, '../files/image')));
 app.use(express.json()); 
 
